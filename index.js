ab2329579314a57db0c925bb732d908f36bdd367
Kneelawk <jedidiah.pommert@gmail.com>
2016-11-16 08:34:00 -0800

    Add request dependency

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
+const request = require('request');
 
 const downloadUrl = 'https://addons.cursecdn.com/files/2231/53/OpenPeripheralIntegration-1.7.10-0.1.0.jar';
 
@@ -12,6 +13,6 @@ if (!fs.existsSync(outputDir)) {
 
 let outputFileName = outputDir + '/' + downloadUrl.slice(downloadUrl.lastIndexOf('/') + 1);
 
-// let outputFile = fs.createWriteStream(outputFileName);
+let outputFile = fs.createWriteStream(outputFileName);
 
 console.log('Downloading to: ' + outputFileName);
