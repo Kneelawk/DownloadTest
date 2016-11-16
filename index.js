23fd4cd3de71d48228db1a2b4400553360f32e8b
Kneelawk <jedidiah.pommert@gmail.com>
2016-11-15 20:52:58 -0800

    Begin on downloader

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+
+const downloadUrl = 'https://addons.cursecdn.com/files/2231/53/OpenPeripheralIntegration-1.7.10-0.1.0.jar';
+
+const outputDir = `${__dirname}/download_output`;
+
+if (!fs.existsSync(outputDir)) {
+  fs.mkdirSync(outputDir);
+}
+
+let outputFileName = outputDir + '/' + downloadUrl.slice(downloadUrl.lastIndexOf('/') + 1);
+
+// let outputFile = fs.createWriteStream(outputFileName);
+
+console.log('Downloading to: ' + outputFileName);
